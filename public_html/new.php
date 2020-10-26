6612330feb7c6c4b2d47ee372d46ac2d728e119a
symphograph <58216294+symphograph@users.noreply.github.com>
2020-10-26 21:20:50 +1100

    new 22

--- public_html/new.php
+++ public_html/new.php
@@ -27,13 +27,9 @@ $ver = random_str(8);
 ?>
 <title><?php echo $p_title;?></title>
     <link rel="icon" href="img/logo/logo.svg" sizes="any" type="image/svg+xml">
-<link href="css/menu.css?ver=<?php echo md5_file($_SERVER['DOCUMENT_ROOT'].'/css/menu.css');?>" rel="stylesheet">
-<link href="css/index.css?ver=<?php echo md5_file($_SERVER['DOCUMENT_ROOT'].'/css/index.css');?>" rel="stylesheet">
-<link href="css/news.css?ver=<?php echo md5_file($_SERVER['DOCUMENT_ROOT'].'/css/menum.css');?>" rel="stylesheet">
-<link href="css/menum.css?ver=<?php echo md5_file($_SERVER['DOCUMENT_ROOT'].'/css/index.css');?>" rel="stylesheet">
-<link href="css/right_nav.css?ver=<?php echo md5_file($_SERVER['DOCUMENT_ROOT'].'/css/right_navcss');?>" rel="stylesheet">
+    <?php CssMeta(['menu.css','index.css','news.css','menum.css', 'right_nav.css'])?>
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
 </head>
 
 <body>
