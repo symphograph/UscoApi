41a777c0fab3a449ba8ae7e242f39862416a6184
symphograph <58216294+symphograph@users.noreply.github.com>
2021-02-17 22:44:33 +1100

    dev class Anonces

--- classes/Anonce.php
+++ classes/Anonce.php
@@ -3,21 +3,58 @@
 
 class Anonce
 {
-    public int $concert_id = 1;
+    public int $ev_id = 0;
     public int $hall_id = 8;
     public string $prog_name = 'Название';
-    public $sdescr;
-    public $description;
-    public $img;
-    public $topimg;
-    public $aftitle;
-    public $datetime;
-    public $pay;
+    public string $sdescr = '';
+    public string $description;
+    public string $img;
+    public string $topimg;
+    public string $aftitle;
+    public string $datetime;
+    public int $pay = 0;
     public int $age;
-    public $ticket_link;
+    public $ticket_link = '';
+    public $hall_name = '';
+    public $payName = 'Условия входа';
+    const PAYS = ['','','Вход свободный','Билеты в продаже','Вход по пригласительным','Билеты в продаже'];
 
-    function byQ($q){
+    function byQ(object $q) : bool
+    {
+        $this->ev_id = $q->ev_id ?? 0;
+        $this->hall_id = $q->hall_id;
+        $this->prog_name = $q->prog_name;
+        $this->sdescr = $q->sdescr;
+        $this->description = $q->description;
+        $this->img = $q->img;
+        $this->topimg = $q->topimg;
+        $this->aftitle = $q->aftitle;
+        $this->datetime = $q->datetime;
+        $this->pay = $q->pay;
+        $this->age = $q->age;
+        $this->ticket_link = $q->ticket_link;
+        $this->hall_name = $q->hall_name;
+    }
 
+    function getdb(int $ev_id){
+        $qwe = qwe("
+            SELECT
+            a.*,
+            a.concert_id as ev_id,
+            h.hall_name,
+            h.map
+            FROM
+            anonces as a
+            INNER JOIN halls as h ON a.hall_id = h.hall_id
+            WHERE a.concert_id = '$ev_id'
+            ");
+
+        if(!$qwe or !$qwe->num_rows)
+            return false;
+
+        $q = mysqli_fetch_object($qwe);
+
+        return self::byQ($q);
     }
 
 }
--- public_html/test.php
+++ public_html/test.php
@@ -15,7 +15,7 @@ if(!$myip) exit;
 
 <body>
 <?php
-echo ROOT;
+
 ?>
 </body>
 </html>
