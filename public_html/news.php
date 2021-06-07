122b157fef2f2407466371fa794ad5ab73039fde
symphograph <58216294+symphograph@users.noreply.github.com>
2021-06-07 15:06:05 +1100

    add news filter

--- public_html/news.php
+++ public_html/news.php
@@ -14,6 +14,7 @@ $ver = random_str(8);
 <title><?php echo $p_title;?></title>
     <link rel="icon" href="img/logo/logo.svg" sizes="any" type="image/svg+xml">
     <?php CssMeta(['menu.css', 'index.css', 'menum.css', 'right_nav.css']) ?>
+    <meta name="robots" content="noindex"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script>
 </head>
