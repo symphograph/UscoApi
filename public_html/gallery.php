154373e794871b4d8df601f759dedda4c54b86f5
symphograph <58216294+symphograph@users.noreply.github.com>
2021-09-01 13:29:58 +1100

    sputnik

--- public_html/gallery.php
+++ public_html/gallery.php
@@ -41,6 +41,13 @@ if(!empty($_GET['up'])){
 		document.querySelector('#gallery').appendChild(JGallery.create([<?php Albums($albumsdir);?>]).getElement());
 	</script>
 </div>
-<script type="text/javascript" src="https://culturaltracking.ru/static/js/spxl.js?pixelId=1032" data-pixel-id="1032"></script>
+<script async src="https://culturaltracking.ru/static/js/spxl.js?pixelId=16513" data-pixel-id="16513"></script>
+<script type="text/javascript">
+    (function(d, t, p) {
+        var j = d.createElement(t); j.async = true; j.type = "text/javascript";
+        j.src = ("https:" == p ? "https:" : "http:") + "//stat.sputnik.ru/cnt.js";
+        var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
+    })(document, "script", document.location.protocol);
+</script>
 </body>
 </html>
