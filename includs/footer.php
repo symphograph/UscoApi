61ba6ceae53dd06abdfaed830b41531ecef98950
symphograph <58216294+symphograph@users.noreply.github.com>
2021-09-01 12:16:54 +1100

    sputnik

--- includs/footer.php
+++ includs/footer.php
@@ -39,6 +39,8 @@
     </div>
     <div class="footcol">
         <p>МУНИЦИПАЛЬНОЕ БЮДЖЕТНОЕ УЧРЕЖДЕНИЕ<br>"ЮЖНО-САХАЛИНСКИЙ КАМЕРНЫЙ ОРКЕСТР"</p>
+        <div id="sputnikArea"><span id="sputnik-informer"></span></div>
+
     </div>
     <div class="footcol">
         <p>УЧРЕДИТЕЛИ</p>
@@ -51,3 +53,10 @@
     </div>
 </div>
 <script async src="https://culturaltracking.ru/static/js/spxl.js?pixelId=16513" data-pixel-id="16513"></script>
+<script type="text/javascript">
+    (function(d, t, p) {
+        var j = d.createElement(t); j.async = true; j.type = "text/javascript";
+        j.src = ("https:" == p ? "https:" : "http:") + "//stat.sputnik.ru/cnt.js";
+        var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
+    })(document, "script", document.location.protocol);
+</script>
--- public_html/index.php
+++ public_html/index.php
@@ -140,12 +140,5 @@ ORDER BY anonces.datetime
 <?php
 include dirname($_SERVER['DOCUMENT_ROOT']).'/includs/footer.php';
 ?>
-<script type="text/javascript">
-    (function(d, t, p) {
-        var j = d.createElement(t); j.async = true; j.type = "text/javascript";
-        j.src = ("https:" == p ? "https:" : "http:") + "//stat.sputnik.ru/cnt.js";
-        var s = d.getElementsByTagName(t)[0]; s.parentNode.insertBefore(j, s);
-    })(document, "script", document.location.protocol);
-</script>
 </body>
 </html>
