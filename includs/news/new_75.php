f14ab0f3bdba2a4a58e717cff1fb548f467c816c
symphograph <58216294+symphograph@users.noreply.github.com>
2021-11-09 21:43:15 +1100

    news filter

--- includs/news/new_75.php
+++ includs/news/new_75.php
@@ -1,17 +1,17 @@
 <?php
 $new_id = 75;
-$imgFolder = 'img/news/' . $new_id;
+$imgFolder = 'img/news/' . $new_id . '/';
 ?>
 <div class="text"><br>
     <br><br>
-    <?php echo (new Img($imgFolder . '/001.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '001.jpg'))->tagArticle() ?>
     <br><br>
     Гостями концерта стали дети и их родители. Ребята с большим восторгом отвечали на каверзные вопросы Тиграна
     Ахназаряна – художественного руководителя и главного дирижера Южно-Сахалинского камерного оркестра. Какой инструмент
     похож на ворчливого дедушку, а какой на хитрую и грациозную кошку, а после того, как дети угадывали или терялись в
     догадках, им на помощь приходили музыканты, исполняя тот или иной мотив на нужном инструменте.
     <br><br>
-    <?php echo (new Img($imgFolder . '/002.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '002.jpg'))->tagArticle() ?>
     <br><br>
     Сказка «Петя и волк» – это музыкальная история, рассказанная камерным оркестром. Текст сказки читали учащиеся
     учебных учреждений города Южно-Сахалинска – из гимназии № 3, Сахалинского колледжа искусств и Сахалинского
@@ -19,13 +19,13 @@ $imgFolder = 'img/news/' . $new_id;
     уникальный «музыкальный портрет». Например, пионер Петя – это мелодия скрипок, Дедушка – фагот, Птичка – флейта в
     высоком регистре, Утка – «крякающий» гобой, Волк – мрачные валторны, Охотники – литавры и большой барабан.
     <br><br>
-    <?php echo (new Img($imgFolder. '/003.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder. '003.jpg'))->tagArticle() ?>
     <br><br>
     Во время концерта юные слушатели не только следили за приключениями мальчика Пети и победой над страшным Волком, но
     и научились распознавать тот или иной музыкальный инструмент, познакомились с его характерными тембровыми
     особенностями. В этом и был педагогический смысл создателей музыкальной сказки – Сергея Прокофьева и Наталии Сац.
     <br><br>
-    <?php echo (new Img($imgFolder . '/004.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '004.jpg'))->tagArticle() ?>
     <br><br>
     Интересный факт – музыкальная сказка «Петя и волк» во всем мире является самым исполняемым сочинением Сергея
     Прокофьева. На сегодняшний день известно порядка 70 записей этого произведения.
@@ -35,19 +35,19 @@ $imgFolder = 'img/news/' . $new_id;
     учреждение «Южно-Сахалинский камерный оркестр» при поддержке компании «Эксон Нефтегаз Лимитед».
     <br><br>
 
-    <?php echo (new Img($imgFolder . '/005.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '005.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/006.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '006.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/007.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '007.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/008.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '008.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/009.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '009.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/010.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '010.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/011.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '011.jpg'))->tagArticle() ?>
     <br><br>
-    <?php echo (new Img($imgFolder . '/012.jpg'))->tagArticle() ?>
+    <?php echo (new Img($imgFolder . '012.jpg'))->tagArticle() ?>
 </div>
