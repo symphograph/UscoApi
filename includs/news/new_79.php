5a1951a7875037ed942b1d3ff21b502f6c266c10
symphograph <58216294+symphograph@users.noreply.github.com>
2021-12-06 14:32:24 +1100

    new 79

---
+++ includs/news/new_79.php
@@ -0,0 +1,10 @@
+<?php
+$new_id    = 79;
+$imgFolder = 'img/news/' . $new_id . '/';
+?>
+<div class="text"><br><br>
+    <a href="https://rodina-dk.ru/translyatsiya-kontserta-yuzhno-sahalinskogo-kamernogo-orkestra/">
+        <?php echo (new Img($imgFolder . '001.jpg'))->tagArticle() ?>
+    </a>
+    <br><br>
+</div>
