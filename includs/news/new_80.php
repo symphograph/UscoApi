b01c24c1dac0577dda966e513a438fe74c3e273e
symphograph <58216294+symphograph@users.noreply.github.com>
2021-12-10 02:37:54 +1100

    new 80

---
+++ includs/news/new_80.php
@@ -0,0 +1,9 @@
+<?php
+$new_id    = 80;
+$imgFolder = 'img/news/' . $new_id . '/';
+?>
+<div class="text"><div style="width: 100vw"></div><br>
+    <br><br>
+    <?php VideoItem('N7xSpGIWFR0');?>
+    <br><br>
+</div>
