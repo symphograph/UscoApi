6691a652cf109390b8f74a8490dbca660904d4b8
symphograph <58216294+symphograph@users.noreply.github.com>
2023-07-28 21:58:36 +1100

    fix rest

---
+++ public_html/api/feedback/send.php
@@ -0,0 +1,8 @@
+<?php
+
+use Symphograph\Bicycle\Api\Response;
+
+require_once dirname($_SERVER['DOCUMENT_ROOT']) . '/vendor/autoload.php';
+
+
+Response::success();
--- public_html/api/news/img/del.php
+++ public_html/api/news/img/del.php
@@ -5,6 +5,7 @@ use App\{APIusco, Entry, User};
 use Symphograph\Bicycle\Api\Response;
 use Symphograph\Bicycle\Errors\AppErr;
 use Symphograph\Bicycle\Errors\ValidationErr;
+use Symphograph\Bicycle\FileHelper;
 
 User::auth([1, 2, 4]);
 
@@ -19,18 +20,12 @@ if (empty($img) or !is_string($img)){
 $file1080 = $_SERVER['DOCUMENT_ROOT'] . Entry::imgFolder . $id . '/' . $img;
 $fileOrig = $_SERVER['DOCUMENT_ROOT'] . '/img/entry/origins/' . $id . '/' . $img;
 
-if (!file_exists($file1080) || is_dir($file1080)){
-    throw new AppErr('file1080 Err', 'Файл 1080 не найден');
+if (FileHelper::fileExists($file1080)){
+    unlink($file1080);
 }
 
-@unlink($file1080) or
-throw new AppErr('file1080 unlink err', 'Ошибка при удалении 1080');
-
-if (!file_exists($fileOrig) || is_dir($fileOrig)){
-    throw new AppErr('fileOrig Err', 'Файл Orig не найден');
+if (FileHelper::fileExists($fileOrig)){
+    unlink($fileOrig);
 }
-
-@unlink($fileOrig) or
-throw new AppErr('fileOrig unlink err', 'Ошибка при удалении Orig');
 
 Response::success();
