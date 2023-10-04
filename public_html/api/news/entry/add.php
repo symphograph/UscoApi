f04dd57e37192c0843382acacecd9884974d4a47
symphograph <58216294+symphograph@users.noreply.github.com>
2023-10-05 01:41:46 +1100

    fix dir

--- public_html/api/news/entry/add.php
+++ public_html/api/news/entry/add.php
@@ -6,7 +6,6 @@ use Symphograph\Bicycle\Api\Response;
 use Symphograph\Bicycle\Errors\AppErr;
 
 User::auth([1, 2, 4]);
-ini_set('max_file_uploads', 100);
 
 $Entry = Entry::addNewEntry() or
 throw new AppErr('addNewEntry err');
