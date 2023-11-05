7d811f28f8a3a1a5804606067125bc1539c016d9
symphograph <58216294+symphograph@users.noreply.github.com>
2023-11-06 10:13:29 +1100

    del trash

--- App/SDT/.env.php
+++
@@ -1,9 +0,0 @@
-<?php
-return (object) [
-    'dbConnect' => (object) [
-        'host' => 'localhost',
-        'name' => 'usco_tapi',
-        'user' => 'usco',
-        'pass' => 'I1mYiL%2m9'
-    ]
-];
