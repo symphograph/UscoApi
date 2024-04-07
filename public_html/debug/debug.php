020d34e847f556e59024255a0ea0d0cf0a6a622b
symphograph <58216294+symphograph@users.noreply.github.com>
2024-04-07 22:41:07 +1100

    add debug

---
+++ public_html/debug/debug.php
@@ -0,0 +1,16 @@
+<?php
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+use Symphograph\Bicycle\Debug\DebugCTRL;
+use Symphograph\Bicycle\Errors\ApiErr;
+use Symphograph\Bicycle\Errors\ValidationErr;
+
+
+if (empty($_POST['method'])) {
+    throw new ValidationErr();
+}
+
+match ($_POST['method']) {
+    'isDebugIp' => DebugCTRL::isDebugIp(),
+    default => throw new ApiErr()
+};
